809b61306df62a85b22fe353061d198696c8a9d5
Dave Joyce <dave@osframework.org>
2018-09-27 22:56:26 -0400

    #1 Spring Initializr generated application scaffolding

---
+++ src/main/java/io/github/davejoyce/boggle/solver/BoggleSolverApplication.java
@@ -0,0 +1,12 @@
+package io.github.davejoyce.boggle.solver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoggleSolverApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BoggleSolverApplication.class, args);
+  }
+}
