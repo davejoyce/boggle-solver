8bfe16afea4fd3721e5350f4fc51396cbe781739
Dave Joyce <dave@osframework.org>
2018-09-29 16:43:59 -0400

    #1 Work on basic i18n for English / French / Spanish

---
+++ src/main/java/io/github/davejoyce/boggle/solver/ApplicationConfig.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2018 David Joyce.
+ *
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/BSD-2-Clause
+ */
+package io.github.davejoyce.boggle.solver;
+
+import io.github.davejoyce.web.servlet.i18n.SmartLocaleResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+import static io.github.davejoyce.boggle.solver.ApplicationConstants.DEFAULT_LOCALE;
+
+/**
+ * Spring application configuration for Boggle Solver.
+ *
+ * @author <a href="dave@osframework.org">Dave Joyce</a>
+ */
+@Configuration
+public class ApplicationConfig implements WebMvcConfigurer {
+
+  @Bean
+  public LocaleResolver localeResolver() {
+    SmartLocaleResolver localeResolver = new SmartLocaleResolver();
+    localeResolver.setDefaultLocale(DEFAULT_LOCALE);
+    return localeResolver;
+  }
+
+  @Bean
+  public LocaleChangeInterceptor localeChangeInterceptor() {
+    LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+    lci.setParamName("lang");
+    return lci;
+  }
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(localeChangeInterceptor());
+  }
+
+}
