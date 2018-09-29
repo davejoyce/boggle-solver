4d7282b746761d98f0fbc8e384082b6dfbf6850e
Dave Joyce <dave@osframework.org>
2018-09-29 14:57:31 -0400

    #1 Added favicon images, updated index template

--- src/main/java/io/github/davejoyce/boggle/solver/controller/BoggleSolverController.java
+++ src/main/java/io/github/davejoyce/boggle/solver/controller/BoggleSolverController.java
@@ -1,5 +1,15 @@
+/*
+ * Copyright (c) 2018 David Joyce.
+ *
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/BSD-2-Clause
+ */
 package io.github.davejoyce.boggle.solver.controller;
 
+import io.github.davejoyce.boggle.solver.model.BoardSize;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,20 +18,29 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Date;
 
+/**
+ * Binds HTTP requests for Boggle Solver URLs to application logic.
+ *
+ * @author <a href="dave@osframwork.org">Dave Joyce</a>
+ */
 @Controller
 public class BoggleSolverController {
+
+  private final Logger logger;
 
   private String appMode;
 
 
   public BoggleSolverController(Environment environment) {
     this.appMode = environment.getProperty("app-mode");
+    this.logger = LoggerFactory.getLogger(getClass());
   }
 
-  @RequestMapping(value = "/", method = RequestMethod.GET)
+  @RequestMapping(value = "/")
   public String index(Model model) {
-    model.addAttribute("timestamp", new Date());
+    model.addAttribute("today", new Date());
     model.addAttribute("mode", appMode);
+    model.addAttribute("boardSizes", BoardSize.values());
 
     return "index";
   }
